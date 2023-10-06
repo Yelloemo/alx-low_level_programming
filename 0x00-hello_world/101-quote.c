8d11810bce30c574dba55d09a3a7684b08be6757
Molyn <mikamolyn002@gmail.com>
2023-10-05 19:42:02 -0700

    Hello World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
-101-quote.c
 #include<stdio.h>
 #include<unistd.h>
+
 /**
 *main- Entry point
 *
@@ -9,7 +9,7 @@
 int main(void)
 {
          "write(2,
-         and that piece of art is useful" - Dora Korpar, 2015-10-19\n",59)
+         and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
          return(1);
 }
 
